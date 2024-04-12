74c796a7ccdd03e7efc49f0d4ed43606994485ca
Hosanahlucas <166649978+Hosanahlucas@users.noreply.github.com>
2024-04-12 16:35:49 -0300

    Atividade Liguagem C
    atividade de fixação, determinar variáveis , usar comandos printf ( para mostrar na tela o comando desejado), scanf (para inputar os dados a serem armazenados na variável determinada, utilizando variáveis com numero inteiro, fracionado.
    utilizando operações de soma, média.

---
+++ Atividade_20032024.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+//Atividade 20-03-2024
+//letra A
+
+	int main ()
+	{
+	
+	int num1;
+			
+	printf("Insira um numero inteiro \n ");
+	scanf("%d", &num1);	
+	num1 = num1-1;
+	printf("o antecessor do numero %d \n" , num1);
+	num1 = num1+2;
+	printf(" o sucessor do numero %d \n" , num1);
+	
+//Letra B
+	
+	float num2;
+	
+	printf("Insira um numero real \n");
+	scanf("%f", &num2);
+	num2 = num2/5;
+	printf("a quinta parte do numero %f \n", num2);
+	
+//Letra c
+
+	int n1,n2,n3,resultado;
+	
+	printf("Insira o numero 1 \n");
+	scanf("%d" , &n1);
+	printf("Insira o numero 2 \n");
+	scanf("%d" , &n2);
+	printf("Insira o numero 3 \n");
+	scanf("%d" , &n3);
+	resultado = n1+n2+n3;
+	printf("a soma dos numeros e: %d \n", resultado);
+	
+//letra D
+
+	float ent1,ent2,ent3,ent4,med;
+	
+	printf("Insira o numero 1 \n");
+	scanf("%f" , &ent1);
+	printf("Insira o numero 2 \n");
+	scanf("%f" , &ent2);
+	printf("Insira o numero 3 \n");
+	scanf("%f" , &ent3);
+	printf("Insira o numero 4 \n");
+	scanf("%f" , &ent4);
+	med = (ent1+ent2+ent3+ent4)/4;
+	printf("a media dos numeros e: %f \n", med);
+	
+//Letra E
+
+	int nasc,ano_atual,idade,result;
+	
+	printf("Insira o ano atual \n");
+	scanf("%d", &nasc);
+	printf("Insira a data de nascimento \n");
+	scanf("%d", &ano_atual);
+	result= nasc-ano_atual;
+	printf("a idade e: %d", result);
+	
+//Letra F
+
+	float
+	 
+}
